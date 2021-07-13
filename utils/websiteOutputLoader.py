ed23ff1b3611c7481c94003dad4657cddf0379d4
Öl-Laptop\Öl <oliverkbeckgmail.com>
2021-07-13 23:58:59 +0200

    no message

--- utils/websiteOutputLoader.py
+++ utils/websiteOutputLoader.py
@@ -14,8 +14,43 @@ class websiteOutputLoader:
         if self.processed_output_path is None:
             self.processed_output_path = dataset_folder_path
 
-    def processData(self, dataset_name):
+    def loadData(self, dataset_name, numOfNeighbours, need_processing=True, contain_sk=True, return_sk=True):
+        """Load the dataset from vadere. Preprocess the dataset if it need processing.
 
+        Args:
+            dataset_name (str): name of the dataset file in the dataset folder.
+            numOfNeighbours (int): number of nearest neighbours
+            need_processing (bool, optional): whether the given dataset need processing. Defaults to True.
+            contain_sk (bool, optional): whether the returned dataset contains the column mean spacing distance sk. Defaults to True.
+            return_sk (bool, optional): whether return the column of mean spacing distance as a single vector.
+
+        Returns:
+            numpy.ndarray: the loaded and processed dataset
+        """
+        numOfCols = 4 + 2 * numOfNeighbours
+
+        if need_processing:
+            self.process_rowdata(dataset_name,numOfNeighbours)
+            dataset_path = os.path.join(self.processed_output_path, "processed_" + dataset_name)
+        else:
+            dataset_path = os.path.join(self.dataset_folder_path, dataset_name)
+
+        tmpRawdata = np.loadtxt(dataset_path, delimiter=" ")
+        mask = np.repeat(True, numOfCols)
+        mask[0] = False
+        mask[1] = False
+        if not contain_sk:
+            mask[3] = False
+
+
+        websiteRawdata = tmpRawdata[:, mask]
+
+        if return_sk:
+            return websiteRawdata, tmpRawdata[:, 3]
+        else:
+            return websiteRawdata
+
+    def process_rowdata(self,dataset_name,numOfNeighbours,need_processing=True,contain_sk=True,return_sk=True):
         dataset_path = os.path.join(self.dataset_folder_path, dataset_name)
         output_path = os.path.join(self.processed_output_path, "processed_" + dataset_name)
 
@@ -25,8 +60,8 @@ class websiteOutputLoader:
         df.columns = ["pid", "frame", "x", "y"]
 
         # add speed and sort by frame
-        x_old, y_old = 0, 0
-        pid_old, frame_old = 0, 0
+
+
         df_speed = pd.DataFrame(columns=['Speed'])
         for index, row in df.iterrows():
             pid, frame, x, y = row["pid"], row["frame"], row['x'], row['y']
@@ -49,14 +84,14 @@ class websiteOutputLoader:
         for i, timeGroup in df.groupby("frame"):
             #print(timeGroup)
             for k, ped in timeGroup.iterrows():
-                kneighbors = PriorityQueue(10)
+                kneighbors = PriorityQueue(numOfNeighbours)
 
                 for l, neighbor in timeGroup.iterrows():
 
                     if neighbor["pid"] == ped["pid"]:
                         continue
                     else:
-                        if kneighbors.qsize() < kneighbors.maxsize:
+                        if kneighbors.qsize() < numOfNeighbours:
                             distance = -((((ped["x"] - neighbor["x"]) ** 2 + (ped["y"] - neighbor["y"])**2)) ** 0.5)/100
                             kneighbors.put([distance, ((neighbor["x"] - ped["x"])/100, (neighbor["y"] - ped["y"])/100)])
                         else:
@@ -69,7 +104,7 @@ class websiteOutputLoader:
 
 
 
-                output.write(self.calculateSk(ped, kneighbors))
+                output.write(self.calculateSk(ped, kneighbors,numOfNeighbours))
         output.close()
 
         return df
@@ -82,19 +117,21 @@ class websiteOutputLoader:
         else:
             return -1
 
-    def calculateSk(self, ped, kneighbors):
+    def calculateSk(self, ped, kneighbors,numOfNeighbours):
+
         stringy = ""
         sk = 0
         size = kneighbors.qsize()
-
+        #print(size)
         # We only want full lines don't we?
-        if size != 10: return ""
+        if size != numOfNeighbours: return ""
+        if ped["speed"] == -1: return ""
 
         for i in range(size):
             [x, (y, z)] = kneighbors.get()
-            stringy = str(y) + " " + str(z) + " "+stringy
+            stringy = " "+str(y) + " " + str(z) + stringy
             sk += -x
         #if(size==0):
         #    return ""
         sk /= size
-        return str(ped["frame"]) + " " + str(ped["pid"]) + " " + str(ped["speed"]) + " " + str(sk) + " " + stringy + "\n"
+        return str(ped["frame"]) + " " + str(ped["pid"]) + " " + str(ped["speed"]) + " " + str(sk) + stringy + "\n"
